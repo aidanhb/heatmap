69cc79e5163ba4c7d0deea85ed44b2e4308e01f3
Aidan Holloway-Bidwell <john@John-Bidwells-MacBook-Pro.local>
2016-12-06 18:12:32 -0500

    Added to script for data fabrication

--- heatmap/data_script.py
+++ heatmap/data_script.py
@@ -1,63 +1,148 @@
 ## generate data for RapidSOS data viz demo
 import numpy as np
 import csv
+import datetime as dt
+import forecastio as fc
+import math
+import random
 
 def temp_maps(lat1, lat2, lon1, lon2, times):
+	maps = {}
+
 	with open('data/temps.csv', 'w') as csvfile:
 		fieldnames = ['time', 'lat', 'lon', 'temp']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-		cells = 50
+		cells = 31
 		dl = (lon2 - lon1) / cells
 		lats = np.arange(lat1, lat2, dl)
 		lons = np.arange(lon1, lon2, dl)
-		width = (lat2 - lat1) * .75
 
 		for time in times:
 			m = np.zeros((len(lats), len(lons)))
-			for lat in lats:
-				for lon in lons:
+			for i, lat in enumerate(lats):
+				for j, lon in enumerate(lons):
 					temp = 54
-					ffront = ffront_fn(lat1, lat2, lon1, lon2, time, times[0], times[-1])(lon)
-					bfront = ffront + width
-					if ffront < lat and lat < bfront:
-						dlat = min(lat - ffront, bfront - lat)
+					ffront = ffront_fn(lat1, lat2, lon1, lon2, time, times[0], times[-1])[0](lon)
+					if ffront < lat:
+						dlat = lat - ffront
 						temp = 28
-						if dlat < cells / 40:
-							temp += (12 * (1 - ((.5 + (np.random.random()) / 2) * (dlat / (cells / 20)))))
 
 					writer.writerow({'time': str(time), 'lat': str(lat), 'lon': str(lon), 'temp': str(temp)})
+					m[i][j] = temp
+
+			maps[time] = m
+
+	return maps
 
 
 
 
-def call_map(lat1, lat2, lon1, lon2, times):
-	return
+def call_map(lat_1, lat_2, lon_1, lon_2, times, weathermaps, house_zones, road_zones):
+	with open('data/calls.csv', 'w') as csvfile:
+		fieldnames = ['time', 'lat', 'lon']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		full_zone = ((lat_1, lon_1), (lat_2, lon_2))
+
+		margin = .1
+
+		for time in times:
+			numcalls = 80
+			coldcalls = 50 * ((54 - weathermaps[time].mean()) / (54 - 28))
+			roadcalls = numcalls * .4
+			housecalls = numcalls * .5
+			randcalls = numcalls * .1
+
+			# Around rush hour
+			if dt.datetime.fromtimestamp(time).hour in [7,9,16,18]:
+				numcalls = 100
+				roadcalls = numcalls * .8
+				housecalls = numcalls * .1
+				randcalls = numcalls * .1
+			# During rush hour
+			elif dt.datetime.fromtimestamp(time).hour in [8,17]:
+				numcalls = 120
+				roadcalls = numcalls * .9
+				housecalls = numcalls * .05
+				randcalls = numcalls * .05
+			# Early morning
+			elif dt.datetime.fromtimestamp(time).hour in [2,3,4,5]:
+				numcalls = 50
+				roadcalls = numcalls * .2
+				housecalls = numcalls * .3
+				randcalls = numcalls * .5
+			# Workday/evening
+			else:
+				pass
+
+			for x in range(0,math.ceil(roadcalls)):
+				road_zone = random.choice(road_zones)
+				lat1, lat2, lon1, lon2 = road_zone[0][0], road_zone[1][0], road_zone[0][1], road_zone[1][1]
+				lat = lat1 + ((lat2 - lat1) * random.random())
+				lon = lon1 + ((lon2 - lon1) * random.random())
+				writer.writerow({'time': str(time), 'lat': str(lat), 'lon': str(lon)})
+
+			for x in range(0,math.ceil(housecalls)):
+				house_zone = random.choice(house_zones)
+				lat1, lat2, lon1, lon2 = house_zone[0][0], house_zone[1][0], house_zone[0][1], house_zone[1][1]
+				lat = lat1 + ((lat2 - lat1) * random.random())
+				lon = lon1 + ((lon2 - lon1) * random.random())
+				writer.writerow({'time': str(time), 'lat': str(lat), 'lon': str(lon)})
+
+			for x in range(0,math.ceil(randcalls)):
+				zone = full_zone
+				lat1, lat2, lon1, lon2 = zone[0][0], zone[1][0], zone[0][1], zone[1][1]
+				lat = lat1 + ((lat2 - lat1) * random.random())
+				lon = lon1 + ((lon2 - lon1) * random.random())
+				writer.writerow({'time': str(time), 'lat': str(lat), 'lon': str(lon)})
+
+			for x in range(0,math.ceil(coldcalls)):
+				max_lon = ffront_fn(lat_1, lat_2, lon_1, lon_2, time, times[0], times[-1])[1](lat_2)
+				zones = [z for z in road_zones if z[1][1] <= max_lon]
+				
+				if zones:
+					zone = random.choice(zones)
+					lon1, lon2 = zone[0][1], zone[1][1]
+					lon = lon1 + ((lon2 - lon1) * random.random())
+					front_intersect = ffront_fn(lat_1, lat_2, lon_1, lon_2, time, times[0], times[-1])[0](lon)
+					lat1, lat2 = max(front_intersect, zone[0][0]), zone[1][0]
+					lat = lat1 + (lat2 - lat1) * random.random()
+					writer.writerow({'time': str(time), 'lat': str(lat), 'lon': str(lon)})
 
 def ffront_fn(lat1, lat2, lon1, lon2, time, start, end):
-	window = 3600 * 18
-	pd = (time % window / window)
+	#window = 3600 * 18
+	#pd = (time % window / window)
+	pd = (end - time) / (end - start)
 	dlon = lon2 - lon1
 	dlat = lat2 - lat1
-	def fn(x):
-		xoffset = lon1 + (pd * dlon)
-		yoffset = lat2 - (pd * dlat)
-		return (x - xoffset)**2 + yoffset
-	return fn
+	xoffset = lon1 + ((1.0 - pd) * dlon)
+	yoffset = lat2 - ((1.0 - pd) * dlat)
+	def fnx(x):
+		return .75 * (x - xoffset) + yoffset
+	def fny(y):
+		return (4.0 / 3.0) * (y - yoffset) + xoffset
+	return fnx, fny
 
 def get_times(start, end):
 	return np.arange(start, end, 3600)
 
 def main():
-	min_lat = 40.841461
-	min_lon = -73.900074
-	max_lat = 40.864605
-	max_lon = -73.839220
-	start = 1479427200
-	end = 1480032000
+	api_key = "d0e27f0fd065049b196bd1668e7d55f7"
+
+	tmp_min_lon, tmp_max_lon, tmp_min_lat, tmp_max_lat = -73.874500000, -73.86, 40.853500, 40.862014
+
+	min_lon, max_lon, min_lat, max_lat = -73.874000, -73.862545, 40.854154, 40.861214
+
+	road_zones = [((40.853870, -73.872458),(40.861396, -73.871675)), ((40.855172, -73.873424), (40.857664, -73.871514)), ((40.856503, -73.869658), (40.858037, -73.862545))]
+	house_zones = [((40.857448, -73.870377), (40.861214, -73.862695)), ((40.854154, -73.869304), (40.856702, -73.862631))]
+
+	start = 1479445200
+	end = 1480050000
 
 	times = get_times(start, end)
-	temp_maps(min_lat, max_lat, min_lon, max_lon, times)
+	print(times)
+	tms = temp_maps(tmp_min_lat, tmp_max_lat, tmp_min_lon, tmp_max_lon, times)
+	call_map(min_lat, max_lat, min_lon, max_lon, times, tms, house_zones, road_zones)
 
 if __name__ == "__main__":
 	main()
--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -6,6 +6,7 @@ import matplotlib.cm as cm
 import matplotlib.colors as mc
 import matplotlib.pyplot as plot
 from matplotlib.collections import PatchCollection
+import matplotlib.font_manager as font
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import pandas as pd
@@ -13,40 +14,46 @@ from pylab import rcParams
 import pyproj
 from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
 import sys
+from scipy.interpolate import griddata
 
+# Different methods will need to behave differently depending on the type of data they're working with.
+# This Enum class makes it clear what sorts of data this class is designed to display.
+# datatype.POINT signifies simple coordinate points without values, over time, such as phone calls
+# datatype.VALUE signifies coordinate points with values attached over time, such as temperature samples
 class datatype(Enum):
-	COUNT = 1
-	VALUES = 2
+	POINT = 1
+	VALUE = 2
 
 class heatmap:
 
-	# Load csv data file of format latitude, longitude, timestamp/n.
+	# Designed to load point-event data with optional value data for overlay.
+	# Load csv data file of format timestamp, latitude, longitude, (and optional value)
 	# Find min and max lat, lon, and time and save values as instance variables.
-	# Return dictionary of lists of values.
-	def load_data(self, file_path, datatype=datatype.COUNT):
-		output = {}
-		output['lat'] = []
-		output['long'] = []
-		output['time'] = []
-		if datatype == datatype.VALUES:
-			output['value'] = []
+	# Return dictionary(s) of lists of values.
+	def load_data(self, point_file_path, value_file_path=None):
+		point_output = {}
+		point_output['lat'] = []
+		point_output['long'] = []
+		point_output['time'] = []
 
-		with open(file_path, 'r') as file:
-			reader = csv.reader(file)
-			for row in reader:
+		value_output = {}
+		value_output['lat'] = []
+		value_output['long'] = []
+		value_output['time'] = []
+		value_output['value'] = []
+
+		with open(point_file_path, 'r') as point_file:
+			point_reader = csv.reader(point_file)
+			for row in point_reader:
 
 				timestamp = int(row[0])
 				lat = float(row[1])
 				lon = float(row[2])
 				time = dt.datetime.fromtimestamp(timestamp)
 
-				output['lat'].append(lat)
-				output['long'].append(lon)
-				output['time'].append(time)
-
-				if datatype == datatype.VALUES:
-					temp = float(row[3])
-					output['value'].append(temp)
+				point_output['lat'].append(lat)
+				point_output['long'].append(lon)
+				point_output['time'].append(time)
 
 				self.min_lat = lat if lat < self.min_lat else self.min_lat
 				self.max_lat = lat if lat > self.max_lat else self.max_lat
@@ -55,22 +62,44 @@ class heatmap:
 				self.first_timestamp = timestamp if timestamp < self.first_timestamp else self.first_timestamp
 				self.last_timestamp = timestamp if timestamp > self.last_timestamp else self.last_timestamp
 
-		return output
+		if value_file_path:
+			with open(value_file_path, 'r') as value_file:
+				value_reader = csv.reader(value_file)
+				for row in value_reader:
+
+					timestamp = int(row[0])
+					lat = float(row[1])
+					lon = float(row[2])
+					time = dt.datetime.fromtimestamp(timestamp)
+					value = float(row[3])
+
+					if self.first_timestamp <= timestamp and timestamp <= self.last_timestamp:
+						value_output['lat'].append(lat)
+						value_output['long'].append(lon)
+						value_output['time'].append(time)
+						value_output['value'].append(value)
+		else:
+			value_output = None
+
+		return point_output, value_output
 
 
 	# Creates Pandas dataframe object with lat and lon columns indexed by time
-	def create_dataframe(self, llt_dict, datatyper=datatype.COUNT):
-		if datatype == datatype.VALUES:
+	def create_dataframe(self, llt_dict, datatype=datatype.POINT):
+		if datatype == datatype.VALUE:
 			df = pd.DataFrame(np.array([llt_dict['lat'], llt_dict['long'], llt_dict['value']]).T, index = llt_dict['time'], columns = ['lat', 'long', 'value'])
 		else:
 			df = pd.DataFrame(np.array([llt_dict['lat'], llt_dict['long']]).T, index = llt_dict['time'], columns = ['lat', 'long'])
 		return df
 
 
-	# Initializes basemap with transverse mercator projection large enough to contain all data points
+	# Initializes basemap with transverse mercator projection large enough to contain all call data points
 	def create_basemap(self):
-		side = max(self.max_long - self.min_long, self.max_lat - self.min_lat)
-		rad = side / 2.0 + side * self.EXTRA
+		xside = self.max_long - self.min_long
+		yside = self.max_lat - self.min_lat
+		buf = max(xside * self.EXTRA, yside * self.EXTRA)
+		xrad = xside / 2.0 + buf
+		yrad = yside / 2.0 + buf 
 		lat_0 = (self.max_lat + self.min_lat) / 2.0
 		lon_0 = (self.max_long + self.min_long) / 2.0
 
@@ -80,10 +109,10 @@ class heatmap:
 				lon_0 = lon_0,
 				lat_0 = lat_0,
 				ellps = 'WGS84',
-				llcrnrlon = lon_0 - rad,
-				llcrnrlat = lat_0 - rad,
-				urcrnrlon = lon_0 + rad,
-				urcrnrlat = lat_0 + rad,
+				llcrnrlon = lon_0 - xrad,
+				llcrnrlat = lat_0 - yrad,
+				urcrnrlon = lon_0 + xrad,
+				urcrnrlat = lat_0 + yrad,
 				lat_ts = 0,
 				resolution = 'i',
 				suppress_ticks = True)
@@ -92,7 +121,7 @@ class heatmap:
 
 
 	# Convenience functions for working with colour ramps and bars. Taken from sensitivecities.com.
-	def colorbar_index(self, ncolors, cmap, labels=None, **kwargs):
+	def colorbar_index(self, mn, mx, ncolors, cmap, labels=None, **kwargs):
 		"""
 		This is a convenience function to stop you making off-by-one errors
 		Takes a standard colour ramp, and discretizes it,
@@ -103,10 +132,10 @@ class heatmap:
 		mappable.set_array([])
 		mappable.set_clim(-.5, ncolors+.5)
 		colorbar = plot.colorbar(mappable, **kwargs)
-		print(ncolors)
-		print(np.linspace(0, ncolors, ncolors + 1))
+		#print(ncolors)
+		#print(np.linspace(0, ncolors, ncolors + 1))
 		colorbar.set_ticks(np.linspace(0, ncolors, ncolors + 1))
-		colorbar.set_ticklabels(np.linspace(0, ncolors, ncolors + 1))
+		colorbar.set_ticklabels(np.linspace(mn, mx, ncolors + 1))
 
 		if labels:
 			colorbar.set_ticklabels(labels)
@@ -137,14 +166,20 @@ class heatmap:
 		return mc.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
 
 
-	#calculate heatmap and points to display for time window (self.start, self.end).
+	# calculate heatmap and points to display for time window (self.start, self.end).
 	# scatter points on basemap and plot heatmap over it.
-	def display(self):
+	def display(self, save_as='data/map.png'):
 		extent = [self.basemap.llcrnrlon, self.basemap.urcrnrlon, self.basemap.llcrnrlat, self.basemap.urcrnrlat]
+		print(extent)
+		cmap = 'cool'
 
 		# setting heatmap to show and getting data points that lie within time window
+		px, py = [], []
+		vx, vy, data, v= None, None, None, None
+
 		self.set_showmap()
-		x, y = self.get_time_window_points()
+
+		px, py = self.get_time_window_points()
 
 		# clear figure and add subplot
 		plot.clf()
@@ -154,60 +189,92 @@ class heatmap:
 		# load arcgis streetmap image into basemap, if none exists. Else show lower res streetmap image.
 		if self.arcgisimage == None:
 			self.arcgisimage = self.basemap.arcgisimage(dpi = 300, service='ESRI_StreetMap_World_2D', xpixels = 12000, zorder = 2)
+			pass
 		else:
 			plot.imshow(self.arcgisimage.make_image(), extent = extent, origin = 'lower', zorder = 2)
 
-		self.scatter = self.basemap.scatter(
-			x,
-			y,
+		self.basemap.scatter(
+			px,
+			py,
 			5, marker='o', lw=.25,
-			facecolor='#ff0000', edgecolor='w',
+			facecolor='#0000ff', edgecolor='w',
 			alpha=0.9, antialiased=True,
 			label='Emergencies', zorder=4)
 
+		if not self.value_dataframe.empty:
+			frame = self.value_dataframes[self.start]
+
+			spatial_resolution = min((self.max_lat - self.min_lat), (self.max_long - self.min_long)) / 20
+	 
+			vx = np.array(frame['long'].tolist())
+			vy = np.array(frame['lat'].tolist())
+			v = np.array(frame['value'].tolist())
+			   
+			yinum = (self.basemap.urcrnrlat - self.basemap.llcrnrlat) / spatial_resolution
+			xinum = (self.basemap.urcrnrlon - self.basemap.llcrnrlon) / spatial_resolution
+			vyi = np.linspace(self.basemap.llcrnrlat, self.basemap.urcrnrlat + spatial_resolution, xinum)
+			vxi = np.linspace(self.basemap.llcrnrlon, self.basemap.urcrnrlon + spatial_resolution, yinum)
+			vxi, vyi = np.meshgrid(vxi, vyi)
+			xi = np.c_[vx.ravel(),vy.ravel()]
+			xx = np.c_[vxi.ravel(),vyi.ravel()]
+			
+			data = griddata((vx, vy), v, (vxi, vyi), method='cubic')
+
+			self.basemap.contourf(
+				vxi,
+				vyi,
+				data,
+				cmap=cmap, alpha=.4, zorder=3, extent=extent)
+		else:
+			plot.imshow(self.showmap, extent=extent, cmap=cmap, alpha=.4, origin='lower', zorder=3)
+
 		# copyright and source data info
-		self.smallprint = ax.text(
-			1.03, 0,
-			'Total points: %s\n$\copyright$ RapidSOS 2016' % self.numpoints,
-			ha='right', va='bottom',
-			size=4,
-			color='#555555',
+		ax.text(
+			.02, .9,
+			'{:%m-%d-%Y\n%H:%M}'.format(self.start, self.numpoints),
+			ha='left', va='bottom',
+			size=18,
+			fontproperties=font.FontProperties(family='sans-serif', weight='bold', size='large'),
+			color='#ffffff',
 			transform=ax.transAxes,
 			zorder = 5)
-
-		cmap = 'plasma'
-		self.heatimage = plot.imshow(self.showmap, extent = extent, cmap = cmap, alpha = .4, origin = 'lower', zorder = 3)
 		# unifinished hexmap implementation.
 		#plot.hexbin(np.array(x), np.array(y), extent = extent, cmap = cmap, alpha = .4, gridsize = self.numcells, edgecolors = 'none')
 
-		cb = self.colorbar_index(ncolors=int(self.showmap.max()), cmap=cmap, shrink=0.5)
-		cb.ax.tick_params(labelsize=6)
+		mn = int(self.showmap.min())
+		mx = int(self.showmap.max())
+		if not self.value_dataframe.empty:
+			mn = int(v.min())
+			mx = int(v.max())
 
-		plot.title("")
+		#cb = self.colorbar_index(mn, mx, mx - mn, cmap=cmap, shrink=0.5)
+		#cb.ax.tick_params(labelsize=6)
+
+		#plot.title("")
 		plot.tight_layout()
-		plot.savefig('data/scatter.png', dpi = 100, alpha = True)
+		plot.savefig(save_as, dpi = 100, alpha = True)
 
 		plot.show()
 
-		cell_mi_dist = self.get_cell_distance('MILES')
-		cell_km_dist = self.get_cell_distance('KILOMETERS')
 		print("Number of points: ", self.numpoints)
-		print("Cell size: ", round(cell_mi_dist, 5), "miles, or ", round(cell_km_dist, 5), " kilometers.")
+		if datatype == datatype.POINT:
+			cell_mi_dist = self.get_cell_distance('MILES')
+			cell_km_dist = self.get_cell_distance('KILOMETERS')
+			print("Cell size: ", round(cell_mi_dist, 5), "miles, or ", round(cell_km_dist, 5), " kilometers.")
 
 
 	# Creates 3D array of per-time-unit (1 day, 1 hour, etc.) layers, each layer an n/m cell georgraphic array, as well as corresponding data frames
 	# per time-unit.
 	# Sets instance variables to these objects
-	def set_heatmaps(self, numcells):
+	def split_data_by_time(self, numcells):
 		self.numcells = numcells
-
-		heatmaps = []
-		dataframes = []
+		xcells = int(numcells)
+		ycells = int((self.basemap.urcrnrlat - self.basemap.llcrnrlat) / ((self.basemap.urcrnrlon - self.basemap.llcrnrlon) / xcells))
 
 		d = dt.timedelta(microseconds=1)
 
-		x = np.linspace(self.basemap.llcrnrlon, self.basemap.urcrnrlon, numcells + 1)
-		y = np.linspace(self.basemap.llcrnrlat, self.basemap.urcrnrlat, numcells + 1)
+		x = np.linspace(self.basemap.llcrnrlon, self.basemap.urcrnrlon, xcells + 1)
+		y = np.linspace(self.basemap.llcrnrlat, self.basemap.urcrnrlat, ycells + 1)
 
 		a, b = 0, 1
 
@@ -217,37 +284,54 @@ class heatmap:
 			end = self.times[b]
 			# Slicing just up to but not including end is necessary since
 			# Pandas is upper bound inclusive
-			points = self.dataframe.loc[start:end - d]
+			points = self.point_dataframe.loc[start:end - d]
 			heatmap = np.zeros((len(y) - 1, len(x) - 1))
 
 			for point in points.itertuples():
 				heatmap[np.searchsorted(y, point[1]) - 1][np.searchsorted(x, point[2]) - 1] += 1
 
-			heatmaps.append(heatmap)
-			dataframes.append(points)
+			self.heatmaps[start] = heatmap
+			self.point_dataframes[start] = points
+
+			if not self.value_dataframe.empty:
+				values = self.value_dataframe.loc[start:end - d]
+				self.value_dataframes[start] = values
 
 			a += 1
 			b += 1
 
-		self.heatmaps = heatmaps
-		self.dataframes = dataframes
+		self.allmaps[numcells] = self.heatmaps
+		self.all_pointframes[numcells] = self.point_dataframes
+		self.all_valueframes[numcells] = self.value_dataframes
 
 
-	# Get lists of x and y coordinates that lie within current time window (self.start, self.end).
+	# Get lists of data coordinates that lie within current time window (self.start, self.end).
 	# Also reset self.numpoints to be the number of points in current time window.
 	def get_time_window_points(self):
+		frames = self.point_dataframes
 
 		points_list = []
-		for i in range(self.start, self.end):
-			if (i < len(self.dataframes)):
-				frame = self.dataframes[i]
+		td = dt.timedelta(hours=1)
+		if self.time_units == 'DAY':
+			td = dt.timedelta(days=1)
+		elif self.time_units == 'MONTH':
+			td = dt.timedelta(months=1)
+
+		curdate = self.start
+		while curdate < self.end:
+			if curdate in frames:
+				frame = frames[curdate]
+				map_points = None
 				map_points = pd.Series([Point(self.basemap(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(frame['long'], frame['lat'])])
+					
 				points_list += list(map_points.values)
 
-		raw_points = MultiPoint(points_list)
-		x = [geom.x for geom in raw_points]
-		y = [geom.y for geom in raw_points]
+			curdate += td
 
+		raw_points = MultiPoint(points_list)
+
+		x = np.array([geom.x for geom in raw_points])
+		y = np.array([geom.y for geom in raw_points])
 		self.numpoints = len(raw_points)
 
 		return x, y
@@ -255,11 +339,21 @@ class heatmap:
 
 	# Set self.showmap to be heatmap corresponding to current time window.
 	def set_showmap(self):
+		xcells = int(self.numcells)
+		ycells = int((self.basemap.urcrnrlat - self.basemap.llcrnrlat) / ((self.basemap.urcrnrlon - self.basemap.llcrnrlon) / xcells))
 
-		heatmap = np.zeros((self.numcells, self.numcells))
-		for i in range(self.start, self.end):
-			if (i < len(self.heatmaps)):
-				heatmap = np.add(heatmap, self.heatmaps[i])
+		td = dt.timedelta(hours=1)
+		if self.time_units == 'DAY':
+			td = dt.timedelta(days=1)
+		elif self.time_units == 'MONTH':
+			td = dt.timedelta(months=1)
+		curdate = self.start
+
+		heatmap = np.zeros((ycells, xcells))
+		while curdate < self.end:
+			if curdate in self.heatmaps:
+				heatmap = np.add(heatmap, self.heatmaps[curdate])
+			curdate += td
 
 		self.showmap = heatmap
 
@@ -267,22 +361,27 @@ class heatmap:
 	# Sets self.times array to new array starting at earliest data point time rounded down to nearest specified time unit,
 	# to latest data point time, rounded up, incrementing by specified time unit.
 	def set_times(self, units):
+		self.time_units = units
+
 		start = dt.datetime.fromtimestamp(self.first_timestamp)
 		end = dt.datetime.fromtimestamp(self.last_timestamp)
 
 		delta = dt.timedelta(days=1)
 
 		if units == 'HOUR':
-			start = start.replace(minute = 0, second = 0)
-			end = end.replace(hour = end.hour + 1, minute = 0, second = 0)
 			delta = dt.timedelta(hours=1)
+			start = start.replace(minute = 0, second = 0)
+			end += delta
+			end = end.replace(minute = 0, second = 0)
 		elif units == 'DAY':
 			start = start.replace(hour = 0, minute = 0, second = 0)
-			end = end.replace(day = end.day + 1, hour = 0, minute = 0, second = 0)
+			end += delta
+			end = end.replace(hour = 0, minute = 0, second = 0)
 		elif units == 'MONTH':
-			start = start.replace(day = 0, hour = 0, minute = 0, second = 0)
-			end = end.replace(month = end.month + 1, day = 0, hour = 0, minute = 0, second = 0)
 			delta = dt.timedelta(months=1)
+			start = start.replace(day = 0, hour = 0, minute = 0, second = 0)
+			end += delta
+			end = end.replace(day = 0, hour = 0, minute = 0, second = 0)
 
 		times = []
 		curr = start
@@ -291,8 +390,8 @@ class heatmap:
 			curr += delta
 
 		self.times = times
-		self.start = 0
-		self.end = len(self.times) - 1
+		self.start = start
+		self.end = self.times[-1]
 
 
 	# Calcualates percentage of data points that get their own cell.
@@ -318,14 +417,14 @@ class heatmap:
 
 
 	def change_numcells(self, numcells):
-		self.set_heatmaps(numcells)
+		self.split_data_by_time(numcells)
 		self.set_showmap()
 		self.display()
 
 
 	def change_time_units(self, units):
 		self.set_times(units)
-		self.set_heatmaps(self.numcells)
+		self.split_data_by_time(self.numcells)
 		self.set_showmap()
 		self.display()
 
@@ -336,47 +435,57 @@ class heatmap:
 		self.display() 
 
 
-	def __init__(self, file_path, time_units='HOUR', winsize=8, datatype=datatype.COUNT):
+	def __init__(self, point_file_path, value_file_path=None, time_units='HOUR', winsize=10):
 		rcParams['figure.figsize'] = winsize, winsize
 
+		# size of map buffer around our data
 		self.EXTRA = 0.01
 
+		# The number of cells in the mesh will be self.numcells x self.numcells
+		# Each cell will be colored based on the count of points that lie within it.
 		self.numcells = 30
-
 		self.ncolors = 5
+
 		self.min_lat = 91
 		self.max_lat = -91
 		self.min_long = 181
 		self.max_long = -181
+
 		self.first_timestamp = sys.maxsize
 		self.last_timestamp = 0
+
 		self.times = []
+		self.time_units = time_units
 		self.start = 0
 		self.end = 0
+
 		# Dictionaries to store computed heatmaps and points for given cell sizes.
 		# Key should be numcells.
 		# Not currently used.
 		self.allmaps = {}
-		self.allframes = {}
+		self.all_pointframes = {}
+		self.all_valueframes = {}
+
 		# array of heatmaps for current cellsize
-		self.heatmaps = []
-		self.dataframes = []
+		self.heatmaps = {}
+		self.point_dataframes = {}
+		self.value_dataframes = {}
+
 		# heatmap for current time window
 		self.showmap = []
 		self.numpoints = 0
 
-		self.data = self.load_data(file_path, datatype)
-		self.dataframe = self.create_dataframe(self.data)
-		self.basemap = self.create_basemap()
+		self.point_data, self.value_data = self.load_data(point_file_path, value_file_path)
+		self.point_dataframe = self.create_dataframe(self.point_data, datatype.POINT)
+		self.value_dataframe = pd.DataFrame()
+		if self.value_data:
+			self.value_dataframe = self.create_dataframe(self.value_data, datatype.VALUE)
 
+		self.basemap = self.create_basemap()
 		self.arcgisimage = None
-		self.scatter = None
-		self.heatimage = None
-		self.smallprint = None
 
 		self.set_times(time_units)
-
-		self.set_heatmaps(self.numcells)
+		self.split_data_by_time(self.numcells)
 
 		self.display()
 
